b9e48be35ba30e2101d36e8a1cd3c33f28251052
Jacob Harris <jacob@jacobharris.org>
2018-05-20 09:29:46 -0400

    Initial Commit

---
+++ Contacts1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contacts1
+//
+//  Created by Jacob Harris on 5/20/18.
+//  Copyright © 2018 Sneeze & Cookie, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contacts1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts1
+//
+//  Created by Jacob Harris on 5/20/18.
+//  Copyright © 2018 Sneeze & Cookie, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
